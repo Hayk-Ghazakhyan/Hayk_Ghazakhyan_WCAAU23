17005ad2f57dd4dcb3b98b84b48ae464ed96bb1b
Hayk <haykghazakhyan@gmail.com>
2024-03-03 00:15:08 +0400

    Sorry Olya for variable names :). I know I annoy you, but it was a bad habit. Before you asked me about docstrings and variable names I already did 3 or 4 homeworks, that was the reason you repeat it every time. I'm sorry, but I corrected all ))

--- Session_3/module_3.py
+++ Session_3/module_3.py
@@ -5,20 +5,24 @@ Matrix = List[List[int]]
 
 
 def task_1(exp: int):
+    # Added wrapped function
     def power(num):
         return num**exp
 
     return power
 
 
-def task_2(*args, **kwags):
+def task_2(*args, **kwargs):
+    # looping into multiple values
     for elem_args in args:
         print(elem_args)
-    for elem_kwargs in kwags.values():
+    # looping into dictionaries
+    for elem_kwargs in kwargs.values():
         print(elem_kwargs)
 
 
 def helper(func):
+    # decorator to add some functionality
     def wrapper(*args, **kwargs):
         print("Hi, friend! What's your name?")
         result = func(*args, **kwargs)
@@ -53,17 +57,14 @@ def task_5(matrix: Matrix) -> Matrix:
     # * is to unpack
     # origin matrix [[1, 2], [3, 4]
     # unpacked [1, 2] [3, 4]
-    # the request is get 2D matrix and it allows use zip, because both list leght are equal
+    # the request is get 2D matrix, and it allows use zip, because both list length are equal
     # zip returns corresponding elements in a tuple
     # (1, 3)
     # (2, 4)
     # list(row) [1, 3] [2, 4]
-    # using in list comrehension we get previous row in a list [[1, 3] [2, 4]]
+    # using in list comprehension we get previous row in a list [[1, 3] [2, 4]]
     transposed = [list(row) for row in zip(*matrix)]
     return transposed
-
-
-print(task_5([[1, 2, 4], [5, 6, 5]]))
 
 
 def task_6(queue: str):
